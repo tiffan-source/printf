9bef97f9db3269bc05dae8efe90429a1f36cf507
abel2319 <abelbakary@gmail.com>
2022-07-13 01:09:20 +0200

    task 2 fifth essaie

--- conversion.c
+++ conversion.c
@@ -7,13 +7,13 @@
  */
 void convert_b(int n, int *p)
 {
-	if (n < 0)
+	/*if (n < 0)
 	{
 		return (-1);
-		/*_putchar('-');
+		_putchar('-');
 		n = -n;
-		++(*p);*/
-	}
+		++(*p);
+	}*/
 	if (n / 2)
 	{	++(*p);
 		convert_b(n / 2, p);
@@ -30,6 +30,8 @@ int binary(int n)
 {
 	int count = 1;
 
+	if (n < 0)
+		return (-1);
 	convert_b(n, &count);
 	return (count);
 }
