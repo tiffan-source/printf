e4f23d9a37509715a1a539e0b70f393cce05b0b3
abel2319 <abelbakary@gmail.com>
2022-07-13 19:15:48 +0200

    first essaie to solve requiement failed

--- main.h
+++ main.h
@@ -13,5 +13,8 @@ int print_string(char *);
 int print_integer(int n);
 int _putchar(char c);
 int binary(int n);
-
+int check_space(const char *str);
+int _printf_r(const char *str, va_list *my_list);
+void convert_b(int n, int *p);
+void print(int n, int *p);
 #endif
